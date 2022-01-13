629505c8cbc847b02cd7049f981009794995ddf3
braxton vangilder <94472968+yoda-braxton@users.noreply.github.com>
2022-01-13 12:36:42 -0600

    Update you.js

--- you.js
+++ you.js
@@ -35,7 +35,7 @@ faudio.addEventListener('timeupdate', function() {
 
 function bookmark() {
 	if ((navigator.appName == "Microsoft Internet Explorer") && (parseInt(navigator.appVersion) >= 4)) {
-		var url = "https://yoda-braxton.github.io/";
+		var url = "https://yoda-braxton.github.io/youare.html";
 		var title = "Idiot!";
 		
 		window.external.AddFavorite(url, title);
